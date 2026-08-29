898ceb9e801b95cbd64e896a0d4173ea63c7e02a
Feyz Ibrahim <276739444+dtexxchange@users.noreply.github.com>
2026-08-29 13:25:48 +0530

    fix: added global api prefix

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -11,6 +11,7 @@ import { TransactionType } from '@prisma/client';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.setGlobalPrefix('api');
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
